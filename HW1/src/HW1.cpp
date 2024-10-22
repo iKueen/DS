74fad3bbc9e39234431c84d812c232629cd1006f
iKueen <102719817+iKueen@users.noreply.github.com>
2024-10-22 23:25:51 +0800

    Update HW1.cpp

--- HW1/src/HW1.cpp
+++ HW1/src/HW1.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int acker(int m,int n)
+// 遞迴計算 Ackermann 函數
+int acker(int m, int n)
 {	
-	if (m== 0) 
+    // 當 m 等於 0，返回 n + 1
+	if (m == 0) 
 		return n + 1;
-	else if (n == 0)
+	else if (n == 0) // 當 n 等於 0，遞迴計算 acker(m-1, 1)
 	{
 		return acker(m - 1, 1);
 	}
-	else
+	else // 否則，遞迴計算 acker(m-1, acker(m, n-1))
 	{
 		return acker(m - 1, acker(m, n - 1));
 	}
-
 }
 
 int main() 
 {
-	cout << acker(1,2);
+    // 測試 Ackermann 函數，輸出結果
+	cout << acker(1, 2); // 計算 A(1, 2)
 
 	return 0;
-
 }
